8412b02a5f698f77b0c9fa02dcbb6ac7c5754372
nshimona <76959876+nshimona@users.noreply.github.com>
2021-04-27 18:21:46 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -130,5 +130,5 @@ def scanner():
     return response
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=9090, debug = True)
+    app.run(host="0.0.0.0", port=9090, debug = False)
     
