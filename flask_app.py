dc9f881e025c1acde9cc744f884a2f606bea9719
nshimona <76959876+nshimona@users.noreply.github.com>
2021-04-27 18:10:13 -0400

    print gpu information

--- flask_app.py
+++ flask_app.py
@@ -52,11 +52,11 @@ class PredictionConfig(Config):
 #     DETECTION_NMS_THRESHOLD= 0.0
 
 ############################################################################
-'''
+
 device_name = tf.test.gpu_device_name()
 if device_name != '/device:GPU:0':
   raise SystemError('GPU device not found')
-print('Found GPU at: {}'.format(device_name))'''
+print('Found GPU at: {}'.format(device_name))
 
 
 cfg = PredictionConfig()
